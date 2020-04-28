9d126c09543f5978df42d597ae1fd198bb1e348d
Lawrence Johnson <lawrence@jtecheng.com>
2020-04-28 14:36:48 -0700

    initial commit

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 # install script for the jtlog and jtlogc programs; run with root privileges.
+# Copyright © 2020 - J-Tech Engineering, Ltd.
+# licensing & permissions {{{
+# install.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+            
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# }}}
 
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo installing applications in /usr/local/bin...
 #cp -v jtlog.py jtlogc.py ti2c.py /usr/local/bin
 install --verbose --backup --target-directory=/usr/local/bin jtlog.py 
 install --verbose --backup --target-directory=/usr/local/bin jtlogc.py 
+install --verbose --backup --target-directory=/usr/local/bin ti2c.py 
 
 # skip symbolic link creation if they exist; they're unlikely to change.
 echo creating symbolic links if required...
